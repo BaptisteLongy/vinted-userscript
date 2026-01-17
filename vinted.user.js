6e55063316bf48c4b41133866ba9d40f49a79433
Baptiste Longy <baptiste.longy@gmail.com>
2026-01-17 13:26:00 +0100

    feat: add a veil preventing interaction while the script is running

--- vinted.user.js
+++ vinted.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vinted Helper for Sonia
 // @namespace    mailto:baptiste.longy@gmail.com
-// @version      0.0.3
+// @version      0.0.4
 // @updateURL    https://github.com/BaptisteLongy/vinted-userscript/raw/refs/heads/master/vinted.user.js
 // @downloadURL  https://github.com/BaptisteLongy/vinted-userscript/raw/refs/heads/master/vinted.user.js
 // @description  My Vinted helper, because Vinted has no API
